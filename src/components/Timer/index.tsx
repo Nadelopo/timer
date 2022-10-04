58d102279e6c13fa4b851c342f1f7295ba89b7b3
Nadelopo <101577205+Nadelopo@users.noreply.github.com>
2022-10-04 22:23:56 +0400

    upd

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,11 +3,11 @@ import { Score } from '../Score'
 import './Timer.sass'
 
 export const Timer = () => {
-  const [isMount, setIsMount] = useState(false)
   const [isInterval, setIsInterval] = useState(0)
+  const [showInputs, setShowInputs] = useState(true)
 
   const [ms, setMs] = useState(78)
-  const [seconds, setSeconds] = useState(1)
+  const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(1)
   const [hours, setHours] = useState(0)
 
@@ -16,8 +16,7 @@ export const Timer = () => {
   }
 
   const setScore = (value: string, type: string) => {
-    const Nvalue = Number(value.slice(0, 2))
-
+    const Nvalue = Number(value)
     switch (type) {
       case 'ms':
         setMs(Nvalue)
@@ -35,17 +34,12 @@ export const Timer = () => {
   }
 
   useEffect(() => {
-    if (isMount) {
-      callTimer()
-    }
-    setIsMount(true)
-  }, [isMount])
-
-  useEffect(() => {
     if (!ms && !seconds && !minutes && !hours) {
       clearInterval(isInterval)
+      setShowInputs(true)
+      setIsInterval(0)
     } else {
-      if (ms === 0) {
+      if (ms === 0 || ms === -1) {
         setMs(99)
         setSeconds(seconds - 1)
       }
@@ -63,49 +57,75 @@ export const Timer = () => {
           setHours(hours - 1)
         }
       }
+      setShowInputs(false)
     }
   }, [ms])
+
+  const stopStartStopwatch = () => {
+    if (isInterval) {
+      clearInterval(isInterval)
+      setIsInterval(0)
+    } else if (ms || seconds || minutes || hours) callTimer()
+  }
+
+  const clear = () => {
+    setMs(0)
+    setSeconds(0)
+    setMinutes(0)
+    setHours(0)
+    clearInterval(isInterval)
+    setIsInterval(0)
+  }
 
   return (
     <div>
       <div className="switch">
-        <button className="btn">start</button>
-        <button className="btn">clear</button>
+        <button className="btn" onClick={stopStartStopwatch}>
+          {isInterval ? 'stop' : 'start'}
+        </button>
+        <button className="btn" onClick={clear}>
+          clear
+        </button>
       </div>
-      <div className="set">
-        <div className="input">
-          <input
-            type="number"
-            onChange={(e) => setScore(e.target.value, 'h')}
-            value={hours}
-            min="0"
-          />
+      {showInputs && (
+        <div className="set">
+          <div className="input">
+            <input
+              type="number"
+              onChange={(e) => setScore(e.target.value, 'h')}
+              value={hours}
+              min="0"
+            />
+          </div>
+          <div className="input">
+            <input
+              type="number"
+              onChange={(e) => setScore(e.target.value, 'm')}
+              value={minutes}
+              min="0"
+              max="60"
+            />
+          </div>
+          <div className="input">
+            <input
+              type="number"
+              onChange={(e) => setScore(e.target.value, 's')}
+              value={seconds}
+              min="0"
+              max="60"
+            />
+          </div>
+          <div className="input">
+            <input
+              type="number"
+              onChange={(e) => setScore(e.target.value, 'ms')}
+              value={ms}
+              min="0"
+              max="99"
+            />
+          </div>
         </div>
-        <div className="input">
-          <input
-            type="number"
-            onChange={(e) => setScore(e.target.value, 'm')}
-            value={minutes}
-            min="0"
-          />
-        </div>
-        <div className="input">
-          <input
-            type="number"
-            onChange={(e) => setScore(e.target.value, 's')}
-            value={seconds}
-            min="0"
-          />
-        </div>
-        <div className="input">
-          <input
-            type="number"
-            onChange={(e) => setScore(e.target.value, 'ms')}
-            value={ms}
-            min="0"
-          />
-        </div>
-      </div>
+      )}
       <Score hours={hours} minutes={minutes} seconds={seconds} ms={ms} />
     </div>
   )
